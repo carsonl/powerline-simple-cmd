538bae3e50c0db3c4d0fcba3fcd22ed795a81f36
Carson Longhorn <carson.longhorn@sony.com>
2018-10-29 22:37:51 -0700

    .

--- powerline-simple-cmd/segments/simple-cmd.py
+++ powerline-simple-cmd/segments/simple-cmd.py
@@ -4,22 +4,7 @@ from __future__ import absolute_import
 import string
 import subprocess
 
-from powerline.theme import requires_segment_info
 
-
-@requires_segment_info
-def cmd(pl, cmd=true):
-    """Return back the vengo environment prompt
-    """
-
-    vengo_env = segment_info['environ'].get('VENGO_ENV')
-    if vengo_env is not None:
-        return os.path.basename(vengo_env)
-from __future__ import absolute_import
-
-
-
-@requires_segment_info
 def cmd(pl, segment_info):
     """Returns a unicode sock, and either an up or down arrow
     """
