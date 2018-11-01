d4d96a17fc8c359dc1c7a83afee56569b50085a8
Carson Longhorn <carson.longhorn@sony.com>
2018-10-31 19:29:13 -0700

    many changes to get this to work

--- powerlinesimplecmd/segments/simplecmd.py
+++ powerlinesimplecmd/segments/simplecmd.py
@@ -1,12 +1,16 @@
-from __future__ import absolute_import
 # vim:fileencoding=utf-8:noet
+from __future__ import absolute_import
 
 import string
 import subprocess
+import powerline.lib.shell
 
 
-def cmd(pl, command):
+def cmd(pl=None, command="/bin/echo None"):
     """Returns a unicode sock, and either an up or down arrow
     """
-    result = subprocess.run(command,stdout=subprocess.PIPE)
-    return result.stdout.decode('utf-8').strip('\n')
+    result = subprocess.getoutput(command)
+    return [{"contents":result.strip('\n'),"highlight_groups":["window"]}]
+
+if __name__ == '__main__':
+    print(cmd(command="/bin/echo test"))
